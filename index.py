432aaf23d89bbcf6f4e5abb8dd8ce0e05423a3d5
Jeyjocar <jeyjoca@hotmail>
2023-05-20 18:12:15 -0500

    Hw2

---
+++ index.py
@@ -0,0 +1,9 @@
+
+#Otra forma de "HELLO WORLD"
+#se crea una carpet y dentro de ella un archivo index.py
+#luego se coloca el print("Hello, World")
+#Para mostran en terminal CTRL+Ñ o nueva terminal
+#Poner el siguiente comando python .\index.py
+
+
+print("Hello, world")
